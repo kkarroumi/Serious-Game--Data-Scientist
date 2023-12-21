12308df7af67eccb8d859c9a89704d0fd649480d
Yassine Karroumi <49875985+kkarroumi@users.noreply.github.com>
2023-12-21 18:00:39 +0100

    V1

---
+++ language.js
@@ -0,0 +1,19 @@
+var currentLang =  "fr";
+var dico = {"fr":[
+        "Votre navigateur n'est peut être pas comptabible. Pour avoir une expérience optimale, nous vous suggérons de télécharger un de ces navigateurs web populaires.",
+        "La lecture des expériences VTS sur mobile n'est pas officiellement supportée. Des erreurs peuvent survenir lors de la lecture de l'expérience.",
+        "Une erreur est survenue lors du chargement de cette expérience. Nous vous suggérons de contacter votre administrateur.",
+        "Votre navigateur ne supporte pas la technologie WebGL ou la lecture de fichier audio. Nous vous suggérons de télécharger un de ces navigateurs web populaires ou contactez votre administrateur.",
+        "Cette expérience est une expérience en réalité virtuelle et votre navigateur ne supporte pas la technologie WebVR. Vous devez lire cette expérience depuis un appareil mobile supportant cette technologie.",
+        "Activer le mode VR",
+    ],
+
+    "en":[
+        "Your browser may not be supported. For an optimal experience, we suggest you download one of these popular web browsers.",
+        "Reading VTS experiences on cell phones is not officially supported. Errors may occur when reading the experience.",
+        "An error occurred while loading this experience. We suggest you contact your administrator.",
+        "Your browser does not support WebGL technology or audio playback. We suggest that you download one of these popular web browsers or contact your administrator.",
+        "This experience is a virtual reality experience and your browser does not support WebVR technology. You must play this experience from a mobile device supporting this technology.",
+        "Activate VR mode",
+    ]};
+
